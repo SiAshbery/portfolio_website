f2273168c5da26607bbcb1bf6a23065682dfb231
Simon Ashbery <si.ashbery@gmail.com>
2018-08-07 20:02:47 +0100

    Adds menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,8 @@
 const menu = document.querySelector(".menu");
 const menuBtn = document.querySelector(".menu-btn");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-item");
+const menuBranding = document.querySelector(".menu-branding");
 
 let hideMenu = true;
 
@@ -7,7 +10,13 @@ menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
   menu.classList.toggle("hide");
+  menuNav.classList.toggle("hide");
+  menuBranding.classList.toggle("hide");
   menuBtn.classList.toggle("close");
+
+  navItems.forEach(item => {
+    item.classList.toggle("hide");
+  });
 
   hideMenu = !hideMenu;
 }
